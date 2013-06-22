6fbfee3b43096c81694cecc18bbb3d328214e2b7
ChristianHoesel <Christian@Christian-PC.fritz.box>
2013-06-22 19:35:49 +0200

    Erste erfolgreiche Verbindung zur MySQL DB hergestellt.

--- de.hoesel.dav.ars/src/de/hoesel/dav/ars/jpa/DefaultArchivData.java
+++ de.hoesel.dav.ars/src/de/hoesel/dav/ars/jpa/DefaultArchivData.java
@@ -11,7 +11,6 @@ import java.util.Date;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
