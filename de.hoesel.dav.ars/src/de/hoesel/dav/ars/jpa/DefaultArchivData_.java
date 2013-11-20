3fd2aa763429d4515915ad697373ae1030800d4f
ChristianHoesel <christianhoesel79@gmail.com>
2013-11-20 21:13:17 +0100

    Toten Code entfernt - Vorlagen aus dem offiziellen Archiv

--- de.hoesel.dav.ars/src/de/hoesel/dav/ars/jpa/DefaultArchivData_.java
+++ de.hoesel.dav.ars/src/de/hoesel/dav/ars/jpa/DefaultArchivData_.java
@@ -7,13 +7,13 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2013-11-17T18:00:56.992+0100")
+@Generated(value="Dali", date="2013-11-20T21:12:08.138+0100")
 @StaticMetamodel(DefaultArchivData.class)
 public class DefaultArchivData_ {
 	public static volatile SingularAttribute<DefaultArchivData, Long> db_id;
 	public static volatile SingularAttribute<DefaultArchivData, SystemObjectArchiv> systemObject;
 	public static volatile SingularAttribute<DefaultArchivData, Date> timestamp;
-	public static volatile SingularAttribute<DefaultArchivData, byte[]> data;
-	public static volatile SingularAttribute<DefaultArchivData, AttributeGroup> atg;
 	public static volatile SingularAttribute<DefaultArchivData, Aspect> asp;
+	public static volatile SingularAttribute<DefaultArchivData, AttributeGroup> atg;
+	public static volatile SingularAttribute<DefaultArchivData, byte[]> data;
 }
